5a6eecd8b6081d97f38a5c02cbda054d7c7fd2f1
David Bernal <bernalde@cmu.edu>
2025-07-07 23:54:46 -0400

    Remove vendor files and add proper .gitignore

--- vendor/bundle/ruby/3.2.0/gems/jekyll-timeago-0.13.1/lib/jekyll-timeago/version.rb
+++
@@ -1,5 +0,0 @@
-module Jekyll
-  module Timeago
-    VERSION = "0.13.1"
-  end
-end
--- vendor/bundle/ruby/3.2.0/gems/metrics-0.12.2/lib/metrics/version.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Released under the MIT License.
-# Copyright, 2021-2024, by Samuel Williams.
-
-module Metrics
-	VERSION = "0.12.2"
-end
