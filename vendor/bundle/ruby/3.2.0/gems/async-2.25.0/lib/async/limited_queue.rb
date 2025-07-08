0fc14214514d1f20bd4e11b66405e0d4cf096ab9
David Bernal <redacted>
2025-07-07 23:54:46 -0400

    Remove vendor files and add proper .gitignore

--- vendor/bundle/ruby/3.2.0/gems/async-2.25.0/lib/async/limited_queue.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# Released under the MIT License.
-# Copyright, 2025, by Samuel Williams.
-
-# The implementation lives in `queue.rb` but later we may move it here for better autoload/inference.
-require_relative "queue"
